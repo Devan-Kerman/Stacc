2dd872bc151acd9d26b62c1d64acf3bb9ec33ba3
Noaaan <noaaan@hotmail.com>
2023-06-07 19:11:45 +0200

    Port 1.20
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/net/devtech/stacc/mixin/DesktopHandSplayedDuplicationFixin.java
+++ src/main/java/net/devtech/stacc/mixin/DesktopHandSplayedDuplicationFixin.java
@@ -14,7 +14,7 @@ import net.minecraft.util.math.MathHelper;
 @Mixin (ScreenHandler.class)
 public class DesktopHandSplayedDuplicationFixin {
 	@Redirect (method = "calculateStackSize", at = @At (value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;floor(F)I"))
-	private static int floor(float input, Set<Slot> slots, int mode, ItemStack stack, int stackSize) {
+	private static int floor(float input, Set<Slot> slots, int mode, ItemStack stack) {
 		return MathHelper.floor((double) stack.getCount() / (double) slots.size());
 	}
 }
